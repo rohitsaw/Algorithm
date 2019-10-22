4f6e03166b382b786007a586235e461a0b5532a8
ROHIT KUMAR SAW <rsaw409@gmail.com>
2019-10-22 23:15:35 +0530

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,6 @@
-import heapq
+
+
+import heapq  #in other language like c++ one can be use STL priority queue
 
 
 def calculate_distances(graph, starting_vertex):
@@ -12,8 +14,8 @@ def calculate_distances(graph, starting_vertex):
     
     
     # heap that is going to proesss ie- vertex adjacent to starting vertex
-    heap = [ (0, starting_vertex) ]
-    heapq.heapify(heap)
+    heap = [ (0, starting_vertex) ]  #push (distance,vertex) in heap
+    heapq.heapify(heap)  #convert list to heap structure internally
     
     while len(heap) > 0:
         
@@ -21,43 +23,26 @@ def calculate_distances(graph, starting_vertex):
         print(distances)
         print()
         
-        # return minimum distance vertex from current_vertex
+        # pop distance and vertex from current_vertex
         current_distance, current_vertex = heapq.heappop(heap)
         
         if current_distance > distances[current_vertex]:
             continue
         
+        # iterating all its neightbour vertex
         for neighbor_vertex, weight in graph[current_vertex].items():
             distance = current_distance + weight
             
+            # if new distance is greater than current then update it and push in queue(may be its neighbour affected)
             if distance < distances[neighbor_vertex]:
                 distances[neighbor_vertex] = distance
                 heapq.heappush(heap, (distance, neighbor_vertex))
                 
                 
     return distances
-        
-# =============================================================================
-#         current_distance, current_vertex = heapq.heappop(pq)
-# 
-#         # Nodes can get added to the priority queue multiple times. We only
-#         # process a vertex the first time we remove it from the priority queue.
-#         if current_distance > distances[current_vertex]:
-#             continue
-# 
-#         for neighbor, weight in graph[current_vertex].items():
-#             distance = current_distance + weight
-# 
-#             # Only consider this new path if it's better than any path we've
-#             # already found.
-#             if distance < distances[neighbor]:
-#                 distances[neighbor] = distance
-#                 heapq.heappush(pq, (distance, neighbor))
-# 
-#     return distances
-# =============================================================================
 
 
+# example graph for testing
 example_graph = {
     'U': {'V': 2, 'W': 5, 'X': 1},
     'V': {'U': 2, 'X': 2, 'W': 3},
